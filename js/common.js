6eaa0c2d33bc9b861b543528ef850e45222dd521
shabashab <shabashab@ukr.net>
2020-02-27 23:17:02 +0200

    fixed js commit

--- js/common.js
+++ js/common.js
@@ -1,29 +1,4 @@
-function distanceToTop(element) {
-    return Math.floor(element.getBoundingClientRect().top);
-}
-
-function scrollToAnchor(anchorId) {
-    const anchor = document.getElementById(anchorId);
-    if(!anchor) return;
-    const anchorTop = distanceToTop(anchor);
-
-    window.scrollBy({top: anchorTop, left: 0, behavior: "smooth"});
-}
-
 window.onload = function() {
     //After page loads
-    const introScrollBtn = document.getElementById("#introScrollBtn");
-    const block1Anchor = document.getElementById("#ib1");
-
-    console.log(introScrollBtn);
-    console.log(block1Anchor);
-
-    introScrollBtn.addEventListener("click", function () {
-       scrollToAnchor(block1Anchor);
-    });
-    
-    window.addEventListener("scroll", function() {
-
-    });
     
 };
