6677cad797a8151ea94183b4cad7a572e6b0c6af
Artem Tarasenko <shabashab@ukr.net>
2020-02-28 09:22:58 +0200

    Added smooth scroll to info block `1

--- js/common.js
+++ js/common.js
@@ -1,10 +1,22 @@
-window.onload = function() {
-    const button = document.getElementById("info1btn");
-    const firstBlock = document.getElementById("firstPage");
+function scroll(el) {
+    const toTop = Math.floor(el.getBoundingClientRect().top);
+    window.scrollBy({ top: toTop, left: 0, behavior: "smooth"});
+}
 
-    button.addEventListener('click', function(e) {
+window.onload = function() {
+    const info1btn = document.getElementById("info1btn");
+    const introBtn = document.getElementById("introScrollBtn");
+
+    const firstBlock = document.getElementById("firstPage");
+    const infoBlock1 = document.getElementById("ib1");
+
+    info1btn.addEventListener('click', function(e) {
         e.preventDefault();
-        const toTop = Math.floor(firstBlock.getBoundingClientRect().top);
-        window.scrollBy({ top: toTop, left: 0, behavior: "smooth"});
+        scroll(firstBlock);
+    });
+
+    introBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        scroll(infoBlock1);
     });
 };
